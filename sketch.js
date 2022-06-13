dda1ba2f9868ad2d88f557056f0d57e4f3a8dbcd
mtwomey19 <mtwomey19@gmail.com>
2022-06-13 09:47:26 -0400

    Add createRowContainer()

---
+++ sketch.js
@@ -0,0 +1,11 @@
+
+// Create a row container
+function createRowContainer() {
+    const body = document.querySelector('body');
+    const rowContainer = document.createElement('div');
+    body.appendChild(rowContainer);   
+}
+
+// Create 4 square divs
+
+// Store 4 square divs inside container
